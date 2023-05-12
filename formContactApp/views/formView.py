b33ad5f76e5789edd02f5c885f8edd7be01041c8
Tu Nombre <tu-correo-electronico>
2023-05-12 04:03:22 -0500

    Contact_Form_Project 6 CRUD con validaciones finales: registros por ciudad y usuario menor de edad

--- formContactApp/views/formView.py
+++ formContactApp/views/formView.py
@@ -4,6 +4,9 @@ from rest_framework.response                    import Response
 from formContactApp.models.form                 import Form
 from formContactApp.serializers.formSerializer  import FormSerializer
 # from django.db  import models
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from rest_framework import serializers
 
 
 #Traer todos los productos
@@ -25,6 +28,24 @@ class FormCreateView(generics.CreateAPIView):
     serializer_class = FormSerializer    
     queryset=Form.objects.all()
 
+    def perform_create(self, serializer):
+        # Obtener los datos del formulario
+        data = serializer.validated_data
+
+        # Validar la fecha de nacimiento
+        fecha_nacimiento = data['fecha_nacimiento']
+        edad = relativedelta(datetime.now(), fecha_nacimiento).years
+        if edad < 18:
+            raise serializers.ValidationError('Lo siento, debes ser mayor de edad para registrarte.')
+
+        # Validar la cantidad de registros por ciudad
+        ciudad = data['ciudad']
+        if Form.objects.filter(ciudad=ciudad).count() >= 3:
+            raise serializers.ValidationError('Lo siento, ya se han registrado 3 personas para esta ciudad.')
+
+        # Crear el registro
+        serializer.save()
+
 #Eliminar un producto        
 class FormDeleteView (generics.DestroyAPIView):
     serializer_class = FormSerializer    
@@ -37,26 +58,3 @@ class FormDeleteView (generics.DestroyAPIView):
 class FormUpdateView(generics.UpdateAPIView):
     serializer_class   = FormSerializer    
     queryset=Form.objects.all()
-    
-
-    # def clean_birth_date(self):
-    #     birth_date = self.cleaned_data['birth_date']
-    #     age = (date.today() - birth_date) // timedelta(days=365.25)
-    #     if age < 18:
-    #         raise form.ValidationError("Debes ser mayor de 18 años para registrarte")
-    #     return birth_date
-
-# class Form(models.Model):
-#     # Definición de campos del modelo
-
-#     # Validación personalizada para no permitir usuarios menores de edad
-#     def clean(self):
-#         if (date.today() - self.fecha_nacimiento).days < 365 * 18:
-#             raise ValidationError("Debe ser mayor de 18 años para registrarse.")
-
-#     # Resto de campos del modelo
-
-# # Señal pre-save para validar la edad antes de guardar el registro
-# @receiver(pre_save, sender=Contact)
-# def validate_age(sender, instance, **kwargs):
-#     instance.clean()
